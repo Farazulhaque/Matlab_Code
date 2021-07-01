d32a680e4ea8292af958ccf3be8352e6c170fdb9
Faraz <farazattari786@gmail.com>
2021-07-01 11:02:55 +0530

    using functions arguments(default) and if-else conditions

---
+++ under_age.m
@@ -0,0 +1,14 @@
+function too_young = under_age(age, limit)
+    // To check for number of arguments passed
+    // if only one arguments is passed then 
+    // set limit to 21 by default
+    if nargin < 2
+      limit = 21;
+    end
+    
+    if age < limit
+        too_young ='true';
+    else
+        too_young = 'false';
+    end
+end
