db2945dc4c320c98d7e68ffc4a4423ca3c14297d
Faraz <farazattari786@gmail.com>
2021-07-14 17:20:35 +0530

    updating spacings

--- guess_number_game.m
+++ guess_number_game.m
@@ -4,7 +4,6 @@ load splat                            % load Chirp followed by a splat music aud
 
 while guess ~= matnum                 % loop until guess number is not equal to generated random number
 sound(y, Fs)                          % play sound
-
     if guess > matnum                 % if guess number is greater than generated random number
         disp ('Too high')             % then print Too high
     else                              % otherwise
@@ -12,6 +11,7 @@ sound(y, Fs)                          % play sound
     end;                              % close if-else
     guess = input('Your next guess please: '); % guess again
 end                                   % end while loop
+
 disp ('At last!');                    % print At last if guess number is equal to generated random number
 load handel                           % load handel Hallelujah chorus music audio
 sound(y, Fs)                          % play sound
